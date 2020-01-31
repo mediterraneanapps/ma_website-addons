c7c42fc247fa0bd094bb25d2c4b135d172da756d
mediterraneanapps <58656212+mediterraneanapps@users.noreply.github.com>
2020-01-31 14:43:58 +0100

    Update __manifest__.py

--- ma_website_sale_product_tags/__manifest__.py
+++ ma_website_sale_product_tags/__manifest__.py
@@ -5,7 +5,7 @@
     'license': 'LGPL-3',
     'category': 'eCommerce',
     "support": "mediterranean.apps@gmail.com",
-    'depends': ['website_sale', 'product_template_tags'],
+    'depends': ['website_sale', 'ma_product_template_tags'],
     'data': [
         'website_sale_product_tags_views.xml',
         'website_sale_product_tags_templates.xml',
